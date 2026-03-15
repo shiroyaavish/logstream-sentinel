2bc991306476bbbdb330f93a094dd431d52cbe05
shiroyaavish <avishshiroya2034@gmail.com>
2026-03-15 12:31:59 +0530

    feat : change port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(process.env.PORT ?? 5050);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
